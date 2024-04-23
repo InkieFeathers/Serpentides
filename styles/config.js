a7f3fd926baa918d01634e782f2427ffef7e891a
InkieFeathers <167272593+InkieFeathers@users.noreply.github.com>
2024-04-22 17:00:14 -0700

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1aAmTvilmbYCPEPGwuhEZ04g-N9Vt59GLHxL_Xa4l7Ro";
 
 
 /* ------------------------------------------------------------------- */
